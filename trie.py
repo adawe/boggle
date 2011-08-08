aa3fc3be387770fbc1a870e0eb7cf387f2cfdbf3
Noel Dawe <Noel.Dawe@cern.ch>
2011-08-08 14:05:12 +0200

    updates

--- trie.py
+++ trie.py
@@ -2,4 +2,13 @@
 class Trie(dict):
 
     def add_word(self, word):
-        pass
+
+        self.__recurse_add_word(word)
+
+    def __recurse_add_word(word, subtree=self):
+
+        if not word:
+            return
+        if word[0] not in subtree:
+            subtree[word[0]] = {}
+        self.__recurse_add_word(word[1:], subtree[word[0]])
