c9975bd50c3e0b133650813b3cb14d06c82c2699
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-10 00:31:41 -0700

    updates

--- boggle.py
+++ boggle.py
@@ -3,6 +3,7 @@
 import string
 import random
 from trie import Trie
+from copy import copy
 
 letters = string.letters[:26]
 
@@ -20,20 +21,43 @@ def fill_table(table):
         for i in range(len(row)):
             row[i] = random.choice(letters)
 
-def recursive_traverse(table, col, row, trie, word="", words=None, visited=None):
+def recursive_traverse(table, col, row, trie, word="", visited=None):
     
-    if words is None:
-        words = []
+    words = []
+    if col < 0 or col >= len(table) \
+       or row < 0 or row >= len(table[0]):
+        return words
     if visited is None:
         visited = []
     if (col, row) not in visited:
         visited.append((col, row))
     else:
         return words
-    # try up
-    # try right
-    # try down
-    # try left
+
+    if table[col][row] in trie:
+        word += table[col][row]
+    else:
+        return words
+    isword, subtrie = trie[table[col][row]]
+    if isword:
+        words.append(word)
+    if subtrie is not None:
+        # try up
+        words += recursive_traverse(table, col, row-1, subtrie, word, copy(visited))
+        # up right
+        words += recursive_traverse(table, col+1, row-1, subtrie, word, copy(visited))
+        # try right
+        words += recursive_traverse(table, col+1, row, subtrie, word, copy(visited))
+        # try down right
+        words += recursive_traverse(table, col+1, row+1, subtrie, word, copy(visited))
+        # try down
+        words += recursive_traverse(table, col, row+1, subtrie, word, copy(visited))
+        # try down left 
+        words += recursive_traverse(table, col-1, row+1, subtrie, word, copy(visited))
+        # try left
+        words += recursive_traverse(table, col-1, row, subtrie, word, copy(visited))
+        # try up left
+        words += recursive_traverse(table, col-1, row-1, subtrie, word, copy(visited))
     return words
 
 fill_table(table)
@@ -45,12 +69,9 @@ dict_file.close()
 trie = Trie()
 map(trie.insert, words)
 
-for word in words:
-    print trie.isword(word)
-
 words = []
 for col in range(len(table)):
-    for row in range(len(col)):
+    for row in range(len(table[0])):
         words += recursive_traverse(table, col, row, trie)
 
 for word in words:
--- trie.py
+++ trie.py
@@ -11,8 +11,10 @@ class Trie(dict):
         if not word:
             return
         if word[0] not in self:
-            self[word[0]] = ((len(word) == 1), Trie(parent=self))
-        self[word[0]][1].insert(word[1:])
+            self[word[0]] = [(len(word) == 1), None]
+        if len(word) > 1:
+            self[word[0]][1] = Trie(parent=self)
+            self[word[0]][1].insert(word[1:])
     
     def __contains__(self, chars):
 
@@ -29,15 +31,3 @@ class Trie(dict):
         if len(chars) == 1:
             return (chars in self) and self[chars][0]
         return (chars[0] in self) and self[chars[0]][1].isword(chars[1:])
-    
-    def down(self, char):
-
-        if char in self:
-            self.marker = char
-            return self[char]
-        return False, None
-    
-    def up(self):
-    
-        self.marker = None
-        return self.parent     
