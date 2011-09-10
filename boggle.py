12a985de034a7e5b65039bc37aaaa27063a8987c
Noel Dawe <Noel.Dawe@cern.ch>
2011-09-09 23:59:20 -0700

    more trie methods

--- boggle.py
+++ boggle.py
@@ -20,13 +20,38 @@ def fill_table(table):
         for i in range(len(row)):
             row[i] = random.choice(letters)
 
+def recursive_traverse(table, col, row, trie, word="", words=None, visited=None):
+    
+    if words is None:
+        words = []
+    if visited is None:
+        visited = []
+    if (col, row) not in visited:
+        visited.append((col, row))
+    else:
+        return words
+    # try up
+    # try right
+    # try down
+    # try left
+    return words
+
 fill_table(table)
 print_table(table)
 
 dict_file = open("dict.txt")
-
 words = [line.strip() for line in dict_file.readlines()]
+dict_file.close()
 trie = Trie()
-for word in words:
-    trie.add_word(word)
+map(trie.insert, words)
 
+for word in words:
+    print trie.isword(word)
+
+words = []
+for col in range(len(table)):
+    for row in range(len(col)):
+        words += recursive_traverse(table, col, row, trie)
+
+for word in words:
+    print word
--- trie.py
+++ trie.py
@@ -1,16 +1,43 @@
 
 class Trie(dict):
 
-    def add_word(self, word):
+    def __init__(self, parent=None):
 
-        self.__recurse_add_word(word)
+        self.parent = parent
+        self.marker = None
 
-    def __recurse_add_word(self, word, subtree=None):
+    def insert(self, word):
 
-        if subtree is None:
-            subtree = self
         if not word:
             return
-        if word[0] not in subtree:
-            subtree[word[0]] = [(len(word) == 1), {}]
-        self.__recurse_add_word(word[1:], subtree[word[0]][1])
+        if word[0] not in self:
+            self[word[0]] = ((len(word) == 1), Trie(parent=self))
+        self[word[0]][1].insert(word[1:])
+    
+    def __contains__(self, chars):
+
+        if len(chars) == 0:
+            return False
+        elif len(chars) == 1:
+            return super(Trie, self).__contains__(chars)
+        return (chars[0] in self) and (chars[1:] in self[chars[0]][1])
+
+    def isword(self, chars):
+
+        if len(chars) == 0:
+            return False
+        if len(chars) == 1:
+            return (chars in self) and self[chars][0]
+        return (chars[0] in self) and self[chars[0]][1].isword(chars[1:])
+    
+    def down(self, char):
+
+        if char in self:
+            self.marker = char
+            return self[char]
+        return False, None
+    
+    def up(self):
+    
+        self.marker = None
+        return self.parent     
